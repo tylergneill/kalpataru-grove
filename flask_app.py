0dbaee7c77f43e455cba8420d32e3a3d7ae93a7d
Tyler Neill <tyler.g.neill@gmail.com>
2025-11-20 14:56:30 -0500

    feat: add overview text, styling, logo, system_diagram stub

--- flask_app.py
+++ flask_app.py
@@ -14,5 +14,9 @@ def index():
         firewatch_url = 'https://firewatch.kalpataru-grove.info'
     return render_template('index.html', firewatch_url=firewatch_url)
 
+@app.route('/system_diagram')
+def system_diagram():
+    return render_template('system_diagram.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
