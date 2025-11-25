ff2e400a74a8e133be9b30c529e8b71d99fed5f5
Tyler Neill <tyler.g.neill@gmail.com>
2025-11-25 11:23:15 -0500

    chore: include app version

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def find_app_version():
+    app_version_filepath = './VERSION'
+    with open(app_version_filepath, 'r', encoding='utf8') as file:
+        # Assuming the __version__ line is the first line
+        return file.readline().strip().split('=')[1].strip().replace("'", "").replace('"', '')
+
 @app.route('/')
 def index():
     env = os.environ.get('ENV')
@@ -12,7 +18,8 @@ def index():
         firewatch_url = 'https://firewatch.kalpataru-grove-stg.dharma.cl'
     else:
         firewatch_url = 'https://firewatch.kalpataru-grove.info'
-    return render_template('index.html', firewatch_url=firewatch_url)
+    app_version = find_app_version()
+    return render_template('index.html', firewatch_url=firewatch_url, app_version=app_version)
 
 @app.route('/system_diagram')
 def system_diagram():
